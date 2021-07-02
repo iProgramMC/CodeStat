18af9fff14aaad396f207a3ecaf83eadea8e7e6b
iProgramInCpp <iprogramincpp@gmail.com>
2021-07-02 16:17:15 +0300

    add source code

---
+++ MainForm.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace CodeStat
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelPath = new System.Windows.Forms.Label();
+            this.InputTextPath = new System.Windows.Forms.TextBox();
+            this.ButtonBrowse = new System.Windows.Forms.Button();
+            this.ButtonCalculate = new System.Windows.Forms.Button();
+            this.CheckIncludeSubdirs = new System.Windows.Forms.CheckBox();
+            this.ConsoleLog = new System.Windows.Forms.ListBox();
+            this.LabelStep = new System.Windows.Forms.Label();
+            this.LabelFile = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.GroupBoxStats = new System.Windows.Forms.GroupBox();
+            this.StatsList = new System.Windows.Forms.ListBox();
+            this.LabelExt = new System.Windows.Forms.Label();
+            this.InputTextExtension = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ButtonCancel = new System.Windows.Forms.Button();
+            this.GroupBoxStats.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LabelPath
+            // 
+            this.LabelPath.AutoSize = true;
+            this.LabelPath.Location = new System.Drawing.Point(12, 17);
+            this.LabelPath.Name = "LabelPath";
+            this.LabelPath.Size = new System.Drawing.Size(32, 13);
+            this.LabelPath.TabIndex = 0;
+            this.LabelPath.Text = "Path:";
+            // 
+            // InputTextPath
+            // 
+            this.InputTextPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputTextPath.Location = new System.Drawing.Point(50, 14);
+            this.InputTextPath.Name = "InputTextPath";
+            this.InputTextPath.Size = new System.Drawing.Size(421, 20);
+            this.InputTextPath.TabIndex = 2;
+            this.InputTextPath.Text = "C:\\Users\\iProgramInCpp\\source\\repos";
+            // 
+            // ButtonBrowse
+            // 
+            this.ButtonBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonBrowse.Location = new System.Drawing.Point(477, 12);
+            this.ButtonBrowse.Name = "ButtonBrowse";
+            this.ButtonBrowse.Size = new System.Drawing.Size(25, 23);
+            this.ButtonBrowse.TabIndex = 3;
+            this.ButtonBrowse.Text = "...";
+            this.ButtonBrowse.UseVisualStyleBackColor = true;
+            this.ButtonBrowse.Click += new System.EventHandler(this.ButtonBrowse_Click);
+            // 
+            // ButtonCalculate
+            // 
+            this.ButtonCalculate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonCalculate.Location = new System.Drawing.Point(12, 405);
+            this.ButtonCalculate.Name = "ButtonCalculate";
+            this.ButtonCalculate.Size = new System.Drawing.Size(490, 44);
+            this.ButtonCalculate.TabIndex = 4;
+            this.ButtonCalculate.Text = "Calculate statistics!";
+            this.ButtonCalculate.UseVisualStyleBackColor = true;
+            this.ButtonCalculate.Click += new System.EventHandler(this.ButtonCalculate_Click);
+            // 
+            // CheckIncludeSubdirs
+            // 
+            this.CheckIncludeSubdirs.AutoSize = true;
+            this.CheckIncludeSubdirs.Checked = true;
+            this.CheckIncludeSubdirs.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CheckIncludeSubdirs.Location = new System.Drawing.Point(15, 40);
+            this.CheckIncludeSubdirs.Name = "CheckIncludeSubdirs";
+            this.CheckIncludeSubdirs.Size = new System.Drawing.Size(147, 17);
+            this.CheckIncludeSubdirs.TabIndex = 5;
+            this.CheckIncludeSubdirs.Text = "Include subdirectories too";
+            this.CheckIncludeSubdirs.UseVisualStyleBackColor = true;
+            // 
+            // ConsoleLog
+            // 
+            this.ConsoleLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ConsoleLog.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConsoleLog.FormattingEnabled = true;
+            this.ConsoleLog.ItemHeight = 14;
+            this.ConsoleLog.Location = new System.Drawing.Point(15, 311);
+            this.ConsoleLog.Name = "ConsoleLog";
+            this.ConsoleLog.Size = new System.Drawing.Size(487, 88);
+            this.ConsoleLog.TabIndex = 6;
+            // 
+            // LabelStep
+            // 
+            this.LabelStep.AutoSize = true;
+            this.LabelStep.Location = new System.Drawing.Point(12, 60);
+            this.LabelStep.Name = "LabelStep";
+            this.LabelStep.Size = new System.Drawing.Size(24, 13);
+            this.LabelStep.TabIndex = 7;
+            this.LabelStep.Text = "Idle";
+            // 
+            // LabelFile
+            // 
+            this.LabelFile.AutoSize = true;
+            this.LabelFile.Location = new System.Drawing.Point(12, 81);
+            this.LabelFile.Name = "LabelFile";
+            this.LabelFile.Size = new System.Drawing.Size(0, 13);
+            this.LabelFile.TabIndex = 8;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(15, 97);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(403, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // GroupBoxStats
+            // 
+            this.GroupBoxStats.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GroupBoxStats.Controls.Add(this.StatsList);
+            this.GroupBoxStats.Location = new System.Drawing.Point(15, 126);
+            this.GroupBoxStats.Name = "GroupBoxStats";
+            this.GroupBoxStats.Size = new System.Drawing.Size(487, 179);
+            this.GroupBoxStats.TabIndex = 10;
+            this.GroupBoxStats.TabStop = false;
+            this.GroupBoxStats.Text = "Statistics";
+            // 
+            // StatsList
+            // 
+            this.StatsList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.StatsList.FormattingEnabled = true;
+            this.StatsList.Location = new System.Drawing.Point(3, 16);
+            this.StatsList.Name = "StatsList";
+            this.StatsList.Size = new System.Drawing.Size(481, 160);
+            this.StatsList.TabIndex = 0;
+            // 
+            // LabelExt
+            // 
+            this.LabelExt.AutoSize = true;
+            this.LabelExt.Location = new System.Drawing.Point(212, 41);
+            this.LabelExt.Name = "LabelExt";
+            this.LabelExt.Size = new System.Drawing.Size(53, 13);
+            this.LabelExt.TabIndex = 11;
+            this.LabelExt.Text = "Extension";
+            // 
+            // InputTextExtension
+            // 
+            this.InputTextExtension.Location = new System.Drawing.Point(271, 38);
+            this.InputTextExtension.MaxLength = 128;
+            this.InputTextExtension.Name = "InputTextExtension";
+            this.InputTextExtension.Size = new System.Drawing.Size(200, 20);
+            this.InputTextExtension.TabIndex = 12;
+            this.InputTextExtension.Text = "cs";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(268, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "(use \'|\' if you want to analyze multiple extensions)";
+            // 
+            // ButtonCancel
+            // 
+            this.ButtonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonCancel.Location = new System.Drawing.Point(424, 97);
+            this.ButtonCancel.Name = "ButtonCancel";
+            this.ButtonCancel.Size = new System.Drawing.Size(75, 23);
+            this.ButtonCancel.TabIndex = 14;
+            this.ButtonCancel.Text = "Cancel";
+            this.ButtonCancel.UseVisualStyleBackColor = true;
+            this.ButtonCancel.Click += new System.EventHandler(this.ButtonCancel_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 461);
+            this.Controls.Add(this.ButtonCancel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InputTextExtension);
+            this.Controls.Add(this.LabelExt);
+            this.Controls.Add(this.GroupBoxStats);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.LabelFile);
+            this.Controls.Add(this.LabelStep);
+            this.Controls.Add(this.ConsoleLog);
+            this.Controls.Add(this.CheckIncludeSubdirs);
+            this.Controls.Add(this.ButtonCalculate);
+            this.Controls.Add(this.ButtonBrowse);
+            this.Controls.Add(this.InputTextPath);
+            this.Controls.Add(this.LabelPath);
+            this.Name = "MainForm";
+            this.Text = "CodeStat V1.0 by iProgramInCpp - Calculate statistics on your code";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.GroupBoxStats.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelPath;
+        private System.Windows.Forms.TextBox InputTextPath;
+        private System.Windows.Forms.Button ButtonBrowse;
+        private System.Windows.Forms.Button ButtonCalculate;
+        private System.Windows.Forms.CheckBox CheckIncludeSubdirs;
+        private System.Windows.Forms.ListBox ConsoleLog;
+        private System.Windows.Forms.Label LabelStep;
+        private System.Windows.Forms.Label LabelFile;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox GroupBoxStats;
+        private System.Windows.Forms.ListBox StatsList;
+        private System.Windows.Forms.Label LabelExt;
+        private System.Windows.Forms.TextBox InputTextExtension;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ButtonCancel;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeStat
+{
+    public partial class MainForm : Form
+    {
+        public bool m_shouldCancel = false;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBrowse_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dlg = new FolderBrowserDialog())
+            {
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    InputTextPath.Text = dlg.SelectedPath;
+                }
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public bool FilePathHasExtensions(string path, string[] exts)
+        {
+            foreach (string ext in exts)
+            {
+                if (path.EndsWith("." + ext)) return true;
+            }
+            return false;
+        }
+        public int FilePathGetExtensionFromList(string path, string[] exts)
+        {
+            for (int i=0; i<exts.Length; i++)
+            {
+                if (path.EndsWith("." + exts[i])) return i;
+            }
+            return -1;
+        }
+        public bool m_runningTask = false;
+        public void GetFiles(ref List<FileThing> files, string path, bool incSubdirs)
+        {
+            string extThing = InputTextExtension.Text;
+            string[] exts = extThing.Split('|');
+            try
+            {
+                string[] filess = Directory.GetFiles(path);
+                if (filess.Length != 0)
+                {
+                    progressBar1.Minimum = 0;
+                    progressBar1.Value = 0;
+                    progressBar1.Maximum = filess.Length + 1;
+                    foreach (string file in filess)
+                    {
+                        if (FilePathHasExtensions(file, exts))
+                        {
+                            if (m_shouldCancel) return;
+                            LabelFile.Text = file;
+                            //files.Add(file);
+                            try
+                            {
+                                FileThing t = new FileThing();
+                                t.fileName = file;
+                                t.fileSize = (int)new FileInfo(file).Length;
+                                string[] lines = File.ReadAllLines(file);
+                                t.loc = lines.Length;
+                                t.sloc = 0;
+                                foreach (var line in lines)
+                                    if (!string.IsNullOrWhiteSpace(line)/*TODO: add comment support too*/)
+                                        t.sloc++;
+                                files.Add(t);
+                            }
+                            catch (Exception ex) { ConsoleLog.Items.Add(ex.Message); }
+                        }
+                        if (m_shouldCancel) return;
+                        progressBar1.Value++;
+                        Application.DoEvents();
+                    }
+                }
+
+                if (incSubdirs)
+                {
+                    string[] subdirs = Directory.GetDirectories(path);
+                    foreach(string subdir in subdirs)
+                    {
+                        if (m_shouldCancel) return;
+                        GetFiles(ref files, subdir, incSubdirs);
+                        if (m_shouldCancel) return;
+                        Application.DoEvents();
+                    }
+                }
+            }
+            catch (Exception ex) { ConsoleLog.Items.Add(ex.Message); }
+            Application.DoEvents();
+        }
+
+        void CalculateTotalStats(ref List<FileThing> fileThings)
+        {
+            int totalLOC = 0, totalSLOC = 0, totalFileSize = 0;
+            var exts = InputTextExtension.Text.Split('|');
+            int extCount = exts.Length;
+            int[] totalLocForExts = new int[extCount];
+            int[] totalSLocForExts = new int[extCount];
+            int[] totalSizeForExts = new int[extCount];
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = fileThings.Count+1;
+            progressBar1.Value = 0;
+            foreach (FileThing thing in fileThings)
+            {
+                if (m_shouldCancel) return;
+                LabelFile.Text = thing.fileName;
+
+                totalLOC += thing.loc;
+                totalSLOC += thing.sloc;
+                totalFileSize += thing.fileSize;
+
+                int ext = FilePathGetExtensionFromList(thing.fileName, exts);
+                if (ext != -1)
+                {
+                    totalLocForExts[ext] += thing.loc;
+                    totalSLocForExts[ext] += thing.sloc;
+                    totalSizeForExts[ext] += thing.fileSize;
+                }
+
+                progressBar1.Value++;
+                if (m_shouldCancel) return;
+                Application.DoEvents();
+            }
+
+            //log everything
+            StatsList.Items.Add($"Total file size: {totalFileSize}");
+            StatsList.Items.Add($"Total non-empty LOC: {totalSLOC}");
+            StatsList.Items.Add($"Total lines of code: {totalLOC}");
+            StatsList.Items.Add("===================================================================");
+            for (int i=0; i<extCount; i++)
+            {
+                StatsList.Items.Add($"[{exts[i]}] total file size: {totalSizeForExts[i]}");
+                StatsList.Items.Add($"[{exts[i]}] total non-empty LOC: {totalSLocForExts[i]}");
+                StatsList.Items.Add($"[{exts[i]}] total lines of code (LOC): {totalLocForExts[i]}");
+            }
+        }
+
+        private void ButtonCalculate_Click(object sender, EventArgs e)
+        {
+            m_runningTask = true;
+            //get files
+            List<FileThing> files = new List<FileThing>();
+
+            StatsList.Items.Clear();
+            LabelStep.Text = "Indexing files...";
+            GetFiles(ref files, InputTextPath.Text, CheckIncludeSubdirs.Checked);
+            LabelStep.Text = "Calculating total stats...";
+            CalculateTotalStats(ref files);
+            LabelStep.Text = "Done!";
+            LabelFile.Text = "";
+            ButtonCancel.Enabled = true;
+            ButtonCancel.Text = "Cancel";
+            m_shouldCancel = false;
+            m_runningTask = false;
+        }
+
+        private void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            if (m_runningTask)
+            {
+                m_shouldCancel = true;
+                ButtonCancel.Enabled = false;
+                ButtonCancel.Text = "Cancelling...";
+            }
+        }
+    }
+    public class FileThing
+    {
+        public string fileName;
+        public int loc, sloc;//lines of code, source lines of code
+        public int fileSize;
+    }
+}
